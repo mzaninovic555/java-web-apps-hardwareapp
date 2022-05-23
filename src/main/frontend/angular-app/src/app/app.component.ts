8b22b9c67f5af177e72918502adf82645d4e0e1e
Marko Zaninović <mzaninovic555@gmail.com>
2022-05-23 18:21:18 +0200

    Dodani moduli Translate, dodani .json fileovi za hrvatski i engleski

--- src/main/frontend/angular-app/src/app/app.component.ts
+++ src/main/frontend/angular-app/src/app/app.component.ts
@@ -1,24 +1,42 @@
-import {Component} from '@angular/core';
-import {AuthenticationService} from "./security/authentication.service";
-import {Router} from "@angular/router";
+import { Component } from '@angular/core';
+import { AuthenticationService } from './security/authentication.service';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Hardware App';
+  currentLanguage: string;
 
   constructor(
     public authenticationService: AuthenticationService,
-    private router: Router
+    private router: Router,
+    private translate: TranslateService
   ) {
+    translate.setDefaultLang('hr');
+    translate.use('hr');
+    this.currentLanguage = translate.currentLang;
+  }
+
+  changeLanguage(): void {
+    switch(this.translate.currentLang){
+      case 'hr':
+        this.translate.use('en');
+        this.currentLanguage = 'en';
+        break;
+      case 'en':
+        this.translate.use('hr');
+        this.currentLanguage = 'hr';
+        break;
+    }
   }
 
   logout() {
     this.authenticationService.logout();
     this.router.navigate(['/login']).then();
   }
-
 }
--- src/main/frontend/angular-app/src/app/app.module.ts
+++ src/main/frontend/angular-app/src/app/app.module.ts
@@ -1,18 +1,28 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HardwaresComponent} from './hardwares/hardwares.component';
-import {HardwareDetailComponent} from './hardware-detail/hardware-detail.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {LoginComponent} from './login/login.component';
-import {FormsModule} from "@angular/forms";
-import {HomeComponent} from './home/home.component';
-import {AuthenticationInterceptor} from "./security/authentication.interceptor";
-import {ForbiddenPageComponent} from "./forbidden-page/forbidden-page.component";
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
-import {HardwareNewComponent} from "./hardware-new/hardware-new.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HardwaresComponent } from './hardwares/hardwares.component';
+import { HardwareDetailComponent } from './hardware-detail/hardware-detail.component';
+import {
+  HTTP_INTERCEPTORS,
+  HttpClientModule,
+  HttpClient,
+} from '@angular/common/http';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './home/home.component';
+import { AuthenticationInterceptor } from './security/authentication.interceptor';
+import { ForbiddenPageComponent } from './forbidden-page/forbidden-page.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { HardwareNewComponent } from './hardware-new/hardware-new.component';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -23,22 +33,29 @@ import {HardwareNewComponent} from "./hardware-new/hardware-new.component";
     LoginComponent,
     HomeComponent,
     ForbiddenPageComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+      defaultLanguage: 'hr',
+    }),
   ],
   providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthenticationInterceptor,
-      multi: true
+      multi: true,
     },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}

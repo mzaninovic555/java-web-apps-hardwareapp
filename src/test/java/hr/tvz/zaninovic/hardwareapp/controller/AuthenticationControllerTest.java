55f4ca83ddcf3d1e92c50d9b608480de7e7558ef
Marko Zaninović <mzaninovic555@gmail.com>
2022-05-31 16:29:36 +0200

    Dodani AuthenticationController testovi.

--- src/test/java/hr/tvz/zaninovic/hardwareapp/controller/AuthenticationControllerTest.java
+++ src/test/java/hr/tvz/zaninovic/hardwareapp/controller/AuthenticationControllerTest.java
@@ -1,17 +1,60 @@
 package hr.tvz.zaninovic.hardwareapp.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.BeforeEach;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class AuthenticationControllerTest {
 
-  @BeforeEach
-  void setUp() {
+  @Autowired
+  private MockMvc mockMvc;
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  private final Map<String, Object> bodyAdmin = Map.of(
+      "username", "admin",
+      "password", "admin"
+  );
+
+  private final Map<String, Object> bodyUser = Map.of(
+      "username", "user",
+      "password", "user"
+  );
+
+  @Test
+  void login_admin() throws Exception {
+    this.mockMvc.perform(
+        post("/authentication/login")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(bodyAdmin)))
+        .andExpect(status().isOk())
+        .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+        .andExpect(content().encoding(StandardCharsets.UTF_8))
+        .andExpect(jsonPath("$.jwt").isNotEmpty());
   }
 
   @Test
-  void login() {
+  void login_user() throws Exception {
+    this.mockMvc.perform(
+            post("/authentication/login")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(bodyUser)))
+        .andExpect(status().isOk())
+        .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+        .andExpect(content().encoding(StandardCharsets.UTF_8))
+        .andExpect(jsonPath("$.jwt").isNotEmpty());
   }
 }
